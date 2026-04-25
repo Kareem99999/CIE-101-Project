bff6f4ac9b3710825a7a0aeef5698bae6f475e6b
Yuosuf ElDeeb <yosifel.deeb714@gmail.com>
2026-04-25 23:04:03 +0300

    Added water well to the Budgetbar

--- UI/BudgetBar.h
+++ UI/BudgetBar.h
@@ -21,7 +21,7 @@ public:
 	virtual void draw() const override;
 	virtual void onClick() = 0;   //The action that should be taken when this icon is clicked
 	int static  getAnimalCounter();
-	void static  increaseAnimals();
+	void static increaseAnimals();
 	void static setRangeMinX(int valueMinX);
 	void static setRangeMaxX(int valueMaxX);
 	void static setRangeMinY(int valueMinY);
@@ -46,6 +46,13 @@ public:
 	virtual void onClick();
 };
 
+class WaterIcon : public BudgetbarIcon
+{
+public:
+	int amount = 0; // counter for water amount
+	WaterIcon(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+	virtual void onClick();
+};
 // TO DO: The rest of icons in the toolbar
 
 enum ANIMAL_ICONS //The icons of the toolbar (you should add more icons)
@@ -57,6 +64,8 @@ enum ANIMAL_ICONS //The icons of the toolbar (you should add more icons)
 	//TODO: Add more icons names here
 	//Cow icon
 	ICON_COW,
+
+	ICON_WATER,
 
 	ANIMAL_COUNT		//no. of menu icons ==> This should be the last line in this enum
 };
