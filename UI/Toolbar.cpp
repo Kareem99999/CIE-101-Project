94c5f55e4d43af1cfbb8f116e011ba90c8935c9a
Omarahmed123458 <138791107+Omarahmed123458@users.noreply.github.com>
2026-04-25 23:42:42 +0300

    Add files via upload

--- UI/Toolbar.cpp
+++ UI/Toolbar.cpp
@@ -14,8 +14,7 @@ void ToolbarIcon::draw() const
 	pWind->DrawImage(image_path, RefPoint.x, RefPoint.y, width, height);
 }
 RestartIcon::RestartIcon(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) : ToolbarIcon(r_pGame, r_point, r_width, r_height, img_path)
-{
-}
+{}
 
 void RestartIcon::onClick()
 {
@@ -50,8 +49,7 @@ void PauseIcon::onClick()
 	//TO DO: add code for pausing the game here
 }
 ExitIcon::ExitIcon(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) : ToolbarIcon(r_pGame, r_point, r_width, r_height, img_path)
-{
-}
+{}
 
 void ExitIcon::onClick()
 {
@@ -62,11 +60,11 @@ Toolbar::Toolbar(Game* r_pGame, point r_point, int r_width, int r_height) : Draw
 {
 	//First prepare List of images for each icon
 	//To control the order of these images in the menu, reoder them in enum ICONS above	
-	//iconsImages[ICON_START] = "images\\Start.jpg";
+	iconsImages[ICON_START] = "images\\Start.jpg";
 	iconsImages[ICON_RESTART] = "images\\RESTART.jpg";
-	//iconsImages[ICON_PAUSE] = "images\\PAUSING.jpg";
-	//iconsImages[ICON_SAVE] = "images\\save.jpg";
-	//iconsImages[ICON_LOAD] = "images\\load.jpg";
+	iconsImages[ICON_PAUSE] = "images\\PAUSING.jpg";
+	iconsImages[ICON_SAVE] = "images\\save.jpg";
+	iconsImages[ICON_LOAD] = "images\\load.jpg";
 	iconsImages[ICON_EXIT] = "images\\EXIT.jpg";
 	point p;
 	p.x = 0;
@@ -75,16 +73,16 @@ Toolbar::Toolbar(Game* r_pGame, point r_point, int r_width, int r_height) : Draw
 	iconsList = new ToolbarIcon * [ICON_COUNT];
 
 	//For each icon in the tool bar create an object 
-	//iconsList[ICON_START] = new StartIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_START]);
-	//p.x += config.iconWidth;
+	iconsList[ICON_START] = new StartIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_START]);
+	p.x += config.iconWidth;
 	iconsList[ICON_RESTART] = new RestartIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_RESTART]);
 	p.x += config.iconWidth;
-	//iconsList[ICON_PAUSE] = new PauseIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_PAUSE]);
-	//p.x += config.iconWidth;
-	//iconsList[ICON_SAVE] = new SaveIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_SAVE]);
-	//p.x += config.iconWidth;
-	//iconsList[ICON_LOAD] = new LoadiIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_LOAD]);
-	//p.x += config.iconWidth;
+	iconsList[ICON_PAUSE] = new PauseIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_PAUSE]);
+	p.x += config.iconWidth;
+	iconsList[ICON_SAVE] = new SaveIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_SAVE]);
+	p.x += config.iconWidth;
+	iconsList[ICON_LOAD] = new LoadiIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_LOAD]);
+	p.x += config.iconWidth;
 	iconsList[ICON_EXIT] = new ExitIcon(pGame, p, config.iconWidth, config.toolBarHeight, iconsImages[ICON_EXIT]);
 	p.x += config.iconWidth;
 }
