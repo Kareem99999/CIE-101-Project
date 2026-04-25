cbd9f754e63a774a7cbb08b1dfff945d85184501
Yuosuf ElDeeb <yosifel.deeb714@gmail.com>
2026-04-25 11:03:58 +0300

    Fixed timer bugs

---
+++ Core/Timer.h
@@ -0,0 +1,15 @@
+#pragma once
+using TIMER = std::chrono::steady_clock;
+using TIME = TIMER::time_point;
+
+class Timer {
+private:
+	TIME start = TIME::min();
+	TIME end = TIME::min();
+public:
+	Timer(int);
+	long long setDuration(int duration);
+	bool check() const;
+	long long elapsed() const;
+	void reset() const;
+};
