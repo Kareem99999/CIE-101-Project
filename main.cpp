e1b82a4f0da5b6de682406169030ea13e4143e61
Yuosuf ElDeeb <yosifel.deeb714@gmail.com>
2026-05-11 23:55:06 +0300

    Added background music

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "./Core/Game.h"
+#include <windows.h>
+#pragma comment(lib, "winmm.lib")
 
 
 int main()
 {
 	bool existing = true;
+	//Plays background music in a loop
+	PlaySound("Sound\\background.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 	while (existing) {
 		//Create an object of controller
 		Game game;
